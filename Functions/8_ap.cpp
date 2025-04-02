8b86c1175494c531b0daee106b0b285e4e97dd46
Shreya <shreya16180@gmail.com>
2025-04-02 23:46:43 +0530

    arithmetic progression

---
+++ Functions/8_ap.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int AP(int n)
+{
+   // int ap = 3*n+7;
+   // return ap;
+   return (3 * n + 7);
+}
+
+int main()
+{
+   int n;
+   cout << "Enter n for Nth term: " << endl;
+   cin >> n;
+
+   // int ap = AP(n);
+   // cout << "Nth term is : " << ap << endl;
+   cout << "Nth term is : " << AP(n) << endl;
+   return 0;
+}
